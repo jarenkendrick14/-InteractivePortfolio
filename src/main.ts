ff74fdf33904279bde3d2a68ca8ba5788799a391
Jaren Kendrick <jarenkendricky@gmail.com>
2025-11-13 10:05:58 +0800

    Upload Angular project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app-module';
 
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app';
+
+
+bootstrapApplication(AppComponent)
   .catch(err => console.error(err));
